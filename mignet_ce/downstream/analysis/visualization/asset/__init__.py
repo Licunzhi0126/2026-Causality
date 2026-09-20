394de4b03609ae203ff76b1c5e1a6c266dca3156
licunzhi <licunzhi0126@gmail.com>
2026-09-20 10:14:16 +0800

    旧版本部分脚本淘汰

--- mignet_ce/downstream/analysis/visualization/asset/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Paper-facing tables and figures built from persisted downstream results."""
-
-from .config import AssetConfig
-from .workflow import build_paper_assets
-
-__all__ = ["AssetConfig", "build_paper_assets"]
